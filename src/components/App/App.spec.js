d3efe9f7074bc0310a8a4c3bb80d0bc2a768922b
muathendirangu <muathe.ndirangu@gmail.com>
2019-05-23 20:34:46 +0300

    add calculator component and unit tests

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Calculator from '../Calculator/Calculator';
 
 describe('App', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<App/>));
+
   it('should render a <div />', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper).toMatchSnapshot();
   });
 })
